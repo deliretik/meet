f05eb7293e97df23b9cdad3b4426ef4c1aeb16e0
deliretik <deliretik@gmail.com>
2022-04-17 16:50:31 +0000

    test showing the list of suggestions during search

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
-//test rendering a list of events
+
 describe('<App /> component', () => {
+    let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+
+    //test rendering a list of events
     test('render list of events', () => {
-        const AppWrapper = shallow(<App />);
         expect(AppWrapper.find(EventList)).toHaveLength(1);
+      });
+
+      //test rendering CitySearch
+      test('render CitySearch', () => {
+        expect(AppWrapper.find(CitySearch)).toHaveLength(1);
       });
 });
 
