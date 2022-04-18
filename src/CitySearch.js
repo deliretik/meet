5167802121cea5489c5d516bacec05c4712bdd36
deliretik <deliretik@gmail.com>
2022-04-18 15:03:37 +0000

    select city from the list of suggestions

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -18,7 +18,12 @@ class CitySearch extends Component {
         });
       };
 
-
+      handleItemClicked = (suggestion) => {
+        this.setState({
+          query: suggestion
+        });
+      }
+      
   render() {
     return (
       <div className="CitySearch">
@@ -30,8 +35,11 @@ class CitySearch extends Component {
       />
       <ul className="suggestions">
         {this.state.suggestions.map((suggestion) => (
-            <li key={suggestion}>{suggestion}</li>
-        ))}
+          <li
+                    key={suggestion}
+                    onClick={() => this.handleItemClicked(suggestion)}
+                  >{suggestion}</li>
+                ))}
             <li key='all'>
                 <b>See all cities</b>
             </li>
