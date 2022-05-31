6937c935ad27b28f54e4511f4ca14b1800519a34
deliretik <deliretik@gmail.com>
2022-05-31 13:50:47 +0200

    created end to end tests

---
+++ src/_tests_/EndToEnd.test.js
@@ -0,0 +1,21 @@
+import puppeteer from 'puppeteer';
+
+describe('show/hide an event details', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      ignoreDefaultArgs: ['--disable-extensions']
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+});
